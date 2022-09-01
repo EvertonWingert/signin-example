8612095680305073904c968d73fce21e695e70c6
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-08-31 22:31:11 -0300

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,28 +1,11 @@
 import { Request, Response } from "express";
-import { UserEntity } from "../entities/user.entity";
 import { AppError } from "../errors/app.error";
-import { IUserRepository } from "../interfaces/user.repository";
 import { UserRepository } from "../repositories/user.repository";
 import { SignInUseCase } from "../usecases/auth/signin.usecase";
 
-class R implements IUserRepository {
-	create(name: string, email: string, password: string): Promise<UserEntity> {
-		throw new Error("Method not implemented.");
-	}
-	update(id: number, user: UserEntity): Promise<void> {
-		throw new Error("Method not implemented.");
-	}
-	delete(id: number): Promise<void> {
-		throw new Error("Method not implemented.");
-	}
-	findByEmail(email: string): Promise<UserEntity | null> {
-		throw new Error("Method not implemented.");
-	}
-}
-
 export class AuthController {
 	async signIn(request: Request, response: Response) {
-		const userRepository = new R();
+		const userRepository = new UserRepository();
 		const signInUseCase = new SignInUseCase(userRepository);
 
 		const { email, password } = request.body;
